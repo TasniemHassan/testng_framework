91076ca98997c90e501d889f7310c3a823506e77
TasniemHassan <tmusa27@gmail.com>
2022-06-04 20:48:24 -0500

    Added TestNg JUnit Soft/Hard Assertions

---
+++ src/test/java/testng_learning/_04_Optional_Assertion_Error_Messages.java
@@ -0,0 +1,24 @@
+package testng_learning;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _04_Optional_Assertion_Error_Messages {
+   /*
+    @Test
+    public void method1(){
+        //TestNG way
+       //Assert.assertTrue(false, "");
+        Assert.assertEquals("", "");
+
+        //JUnit way
+        Assert.assertTrue("", false);
+        Assert.assertEquals("","", "");
+
+    }
+
+    */
+
+
+
+}
